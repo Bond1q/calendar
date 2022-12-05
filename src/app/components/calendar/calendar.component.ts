b14ae978378b76cbe4d47e4e2866038827886c0d
Bondi <kirkizon5@gmail.com>
2022-12-05 12:34:14 +0200

    prettier component

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -2,9 +2,12 @@ import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 import * as moment from 'moment';
-import { AbsencePeriod, AbsencePeriodInCalendar, DateInfo, AbsenceTypes } from '../../types/types'
-
-
+import {
+	AbsencePeriod,
+	AbsencePeriodInCalendar,
+	DateInfo,
+	AbsenceTypes,
+} from '../../types/types';
 
 const list: AbsencePeriod[] = [
 	{
@@ -33,7 +36,6 @@ const list: AbsencePeriod[] = [
 	},
 ];
 
-
 @Component({
 	selector: 'app-calendar',
 	templateUrl: './calendar.component.html',
@@ -41,35 +43,33 @@ const list: AbsencePeriod[] = [
 })
 export class CalendarComponent implements OnInit {
 	datesInfo: DateInfo[] = [];
-	readonly days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-	readonly absenceTypes: AbsenceTypes[] = ['sick', 'vacation']
-
 	date = new Date();
 	absenceList = list;
-	toppings = this._formBuilder.group(this.absenceTypes.reduce((acc: { [key: string]: boolean }, item) => {
-		acc[item] = true;
-		return acc;
-	}, {}));
 
+	readonly days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+	readonly absenceTypes: AbsenceTypes[] = ['sick', 'vacation'];
 
+	toppings = this._formBuilder.group(
+		this.absenceTypes.reduce((acc: { [key: string]: boolean }, item) => {
+			acc[item] = true;
+			return acc;
+		}, {})
+	);
 
 	constructor(private _formBuilder: FormBuilder) { }
-
-
-	onTypeClick() {
-		console.log(this.toppings.value);
-
-
-
-	}
-
-
 
 	setAbsenceDays(dates: Date[]): DateInfo[] {
 		return dates.map((date) => {
 			const newAbsenceList = this.absenceList.reduce((prev, item) => {
-				if (moment(date).isBetween(moment(item.dateStart), moment(item.dateEnd), undefined, '[]') && this.toppings.value[item.type]) {
-
+				if (
+					moment(date).isBetween(
+						moment(item.dateStart),
+						moment(item.dateEnd),
+						undefined,
+						'[]'
+					) &&
+					this.toppings.value[item.type]
+				) {
 					prev.push({ type: item.type, title: item.comment });
 				}
 				return prev;
@@ -97,10 +97,9 @@ export class CalendarComponent implements OnInit {
 	backToToday() {
 		this.date = moment().toDate();
 		this.datesInfo = this.setAbsenceDays(this.getCalendarDays(this.date));
-
 	}
 
-	private getCalendarDays(date = new Date()) {
+	getCalendarDays(date = new Date()) {
 		const calendarStartTime = this.getCalendarStartDay(date);
 
 		return Array.from(Array(42).keys()).map((num) => {
@@ -108,7 +107,7 @@ export class CalendarComponent implements OnInit {
 		});
 	}
 
-	private getCalendarStartDay(date = new Date()) {
+	getCalendarStartDay(date = new Date()) {
 		const year = moment(date).year();
 		const month = moment(date).month();
 		console.log(year);
@@ -122,9 +121,8 @@ export class CalendarComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-		this.toppings.valueChanges.subscribe(values => {
+		this.toppings.valueChanges.subscribe((values) => {
 			this.datesInfo = this.setAbsenceDays(this.getCalendarDays(this.date));
-
 		});
 		this.datesInfo = this.setAbsenceDays(this.getCalendarDays(this.date));
 	}
