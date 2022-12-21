c3ca02ba997c2672f0334ad0b9a50d8bca3cf633
Bondi <kirkizon5@gmail.com>
2022-12-21 14:28:48 +0200

    formatting fix

--- apps/client/src/app/store/selectors/absence.selector.ts
+++ apps/client/src/app/store/selectors/absence.selector.ts
@@ -2,12 +2,5 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AbsenceReducer } from '../../types/types';
 
 export const featureSelector = createFeatureSelector<AbsenceReducer>('absence');
-export const absencesSelector = createSelector(
-	featureSelector,
-	(state) => state.absencePeriods
-);
-
-export const loadingSelector = createSelector(
-	featureSelector,
-	(state) => state.loading
-)
+export const absencesSelector = createSelector(featureSelector, (state) => state.absencePeriods);
+export const loadingSelector = createSelector(featureSelector, (state) => state.loading);
